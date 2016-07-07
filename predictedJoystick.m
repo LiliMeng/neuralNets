73946872147ecb783c21ac40ed42b5e23c94523e
LiliMeng <lilimeng1103@gmail.com>
2016-07-07 11:37:19 -0700

    Create predictedJoystick.m

---
+++ predictedJoystick.m
@@ -0,0 +1,12 @@
+load('pedictedJoystickLR.txt')
+load('pedictedJoystickBF.txt')
+joyLR=pedictedJoystickLR(:,1);
+joyBF=pedictedJoystickBF(:,1);
+
+N=size(joyLR(:,1),1);
+
+fileID3 = fopen('/home/lili/workspace/RNN/data/freq16/predictedJoystick.txt', 'w');
+
+for i=1:N
+    fprintf(fileID3, '%s %s %s\n', num2str(joyLR(i)),',',num2str(joyBF(i)));
+end
